ec5dbe242b85577c2e2aa211a10602331eb6a9b1
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2017-09-01 14:47:22 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    if (33444.5556656)      
+        printf("Inside if: floating point constant val but 0.0 is"
+               " treated as TRUE i.e NON-ZERO VALUE in if condition!!\n");
+    return 0;
+}
