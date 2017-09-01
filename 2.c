ad90b2686757a2ab5a4ebc2c77bf89ee525e0a6e
kumaransabapathy <31533930+kumaransabapathy@users.noreply.github.com>
2017-09-01 14:34:12 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+     if(n % 2 == 0)
+        printf("%d is even.", number);
+    else
+        printf("%d is odd.", number);
+
+    return 0;
+}
